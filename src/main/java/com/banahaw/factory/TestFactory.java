9f36c799132f23b5bd2c9b12f4b240edd138f895
Rod Fetalvero <botsot.felix@gmail.com>
2022-11-10 19:11:54 +0800

    Rename testclass

--- src/main/java/com/banahaw/factory/TestFactory.java
+++ src/main/java/com/banahaw/factory/TestFactory.java
@@ -1,6 +1,6 @@
 package com.banahaw.factory;
 
-public class Main {
+public class TestFactory {
     public static void main(String[] args) {
         Laptop min = LaptopFactory.getSpec("min", 8, 256, "i5-12450Hz");
         Laptop reco = LaptopFactory.getSpec("reco", 16, 512, "i7-12700Hz");
